9c181905139ac2cf1d322d0ccbf0cfec028fef55
Hexirp <Hexirp@users.noreply.github.com>
2021-03-25 21:17:46 +0900

    Add: fromListToMatrix の一部を
    必要な関数であるため。

--- src/BMS_4.elm
+++ src/BMS_4.elm
@@ -2,6 +2,8 @@ module BMS_4 exposing (Matrix, expand)
 
 import Basics exposing (..)
 
+import List exposing (List)
+
 import Array exposing (Array)
 
 {-| これは自然数です。 -}
@@ -9,6 +11,20 @@ type Nat = Nat Int
 
 {-| これはバシク行列システムにおける行列です。 -}
 type Matrix = Matrix Int Int (Array (Array Int))
+
+{-| 或るリストを或る行列へと変換します。 -}
+fromListToMatrix : List (List Int) -> Matrix
+fromListToMatrix x_y_list
+  =
+    let x = List.length x_y_list in
+      case fromListToMatrix_helper x_y_list of
+        (y, generator) -> Matrix x y (generator y)
+
+fromListToMatrix_helper : List (List Int) -> (Int, Int -> Array (Array Int))
+fromListToMatrix_helper x_y_list
+  =
+    case x_y_list of
+      [] -> (0, \n -> Array.empty)
 
 {-| 或る行列を或る自然数により展開します。 `Just` で包んだ結果を返します。其の自然数が其の行列の共終タイプ以上なら `Nothing` を返します。 -}
 expand : Matrix -> Nat -> Maybe Matrix
