1d5530f87d6b365c40cc63b05df6ce266a92974f
Hexirp <Hexirp@users.noreply.github.com>
2021-03-25 22:50:15 +0900

    Update: 仕様をきちんと考える
    それに伴って、仕様に合わせてコードを変更した。

--- src/BMS_4.elm
+++ src/BMS_4.elm
@@ -11,20 +11,25 @@ import Array exposing (Array)
 {-| これは自然数です。 -}
 type Nat = Nat Int
 
-{-| これはバシク行列システムにおける行列です。 -}
+{-| これはバシク行列システムにおける行列。 -}
 type Matrix = Matrix Int Int (Array (Array Int))
 
-{-| 或るリストを或る行列へと変換します。 -}
+{-| 或るリストを或る行列へと変換します。
+
+其のリストが行列を正しく表していれば、そのようになります。もし、列の長さが不揃いであれば、最も長い列が行数の基準となり、それに合わせて他の列が底値でもってパディングされます。もし、行の長さがゼロであれば、行数（列の長さ）は不定値になりますので、デフォルトの値としてゼロとなります。
+
+底値は、其のリストに含まれる最小の値です。ゼロを使わないのは、其のリストが表現する行列のトポロジーを可能な限り保つためです。
+-}
 fromListToMatrix : List (List Int) -> Matrix
 fromListToMatrix x_y_list
   =
     let
       x = List.length x_y_list
-      y = (Maybe.withDefault 0 << List.maximum) (List.map List.length x_y_list)
+      y = Maybe.withDefault 0 (List.maximum (List.map List.length x_y_list))
       e
         =
-          (Maybe.withDefault 0 << List.minimum)
-            (List.map (Maybe.withDefault 0 << List.minimum) x_y_list)
+          Maybe.withDefault 0
+            (List.minimum (List.filterMap List.minimum x_y_list))
       a = Array.fromList (List.map (fromListToMatrix_helper_1 y e) x_y_list)
     in
       Matrix x y a
