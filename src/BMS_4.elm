38f5b730bcf6d8f2c2a79b69935116e20f81cffd
Hexirp <Hexirp@users.noreply.github.com>
2021-03-25 20:32:04 +0900

    Update: 計算の例を削除する
    まだ計算の例を表現するための関数が足りないため。

--- src/BMS_4.elm
+++ src/BMS_4.elm
@@ -7,15 +7,6 @@ import Array exposing (Array)
 {-| Representation of matrixes, the notion on Bashicu Matrix System. -}
 type Matrix = Matrix Int Int (Array (Array Int))
 
-{-| Expand a matrix with an integer. Return `Just` the result. Return `Nothing` if the cofinal type is less than the integer. Return `Nothing` if the integer is negative.
-
-  map (expand 2) (fromList [[0,0,0],[1,1,1]]) == map Just (fromList [[0,0],[1,1],[2,2]])
-  map (expand 0) (fromList [[0],[1],[2]]) == map Just (fromList [[0],[1]])
-  map (expand 2) (fromList [[0],[1],[2]]) == map Just (fromList [[0],[1],[1],[1])
-  map (expand 0) (fromList [[0],[0]]) == map Just (fromList [[0],[0]])
-  map (expand 1) (fromList [[0],[0]]) == Just Nothing
-  map (expand 0) (fromList []) == Just Nothing
-  map (expand -1) (fromList [[0],[1],[2]]) == Just Nothing
--}
-expand : Int -> Matrix -> Maybe Matrix
-expand n x = Just x
+{-| Expand a matrix with an integer. Return `Just` the result. Return `Nothing` if the cofinal type is less than the integer. Return `Nothing` if the integer is negative. -}
+expand : Matrix -> Int -> Maybe Matrix
+expand n x = expand n x
