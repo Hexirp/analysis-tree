52c7b5169072ac1dd9ac2e3840192407094a4b7d
Hexirp <Hexirp@users.noreply.github.com>
2021-03-25 21:00:45 +0900

    Add: パトリックス関連を
    共終タイプの計算の実装を進めるため。

--- src/BMS_4.elm
+++ src/BMS_4.elm
@@ -13,3 +13,13 @@ type Matrix = Matrix Int Int (Array (Array Int))
 {-| 或る行列を或る自然数により展開します。 `Just` で包んだ結果を返します。其の自然数が其の行列の共終タイプ以上なら `Nothing` を返します。 -}
 expand : Matrix -> Nat -> Maybe Matrix
 expand n x = expand n x
+
+{-| これはピンデックスです。ピンデックスは或る行列の要素へのポインターを意味します。 -}
+type Pindex = Null | Pindex Int
+
+{-| これはパトリックスです。パトリックスはピンデックスの行列を意味します。 -}
+type Patrix = Potrix Int Int (Array (Array Pindex))
+
+{-| 或る行列をパトリックスへ変換します。 -}
+fromMatrixToPatrix : Matrix -> Patrix
+fromMatrixToPatrix x = fromMatrixToPatrix x
