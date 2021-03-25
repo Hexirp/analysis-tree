b600843682d1adc9cc50efa9999c730fac2f43bb
Hexirp <Hexirp@users.noreply.github.com>
2021-03-25 19:05:59 +0900

    Update: BMS 4 関連のコードを分離する
    時期尚早の分割は Elm Guide でも警告されているけど、これは間違いなく
    分割したほうがいいケースであるため。

--- src/Main.elm
+++ src/Main.elm
@@ -2,29 +2,13 @@ module Main exposing (Model, Message, initialize, view, update, main)
 
 import Basics exposing (..)
 
-import Array exposing (Array)
+import BMS_4
 
 import Html exposing (Html, div, input, button, text)
 import Html.Attributes exposing (class, placeholder, value)
 import Html.Events exposing (onClick, onInput)
 
 import Browser
-
-{-| Representation of matrixes, the notion on Bashicu Matrix System. -}
-type Matrix = Matrix Int Int (Array (Array Int))
-
-{-| Expand a matrix with an integer. Return `Just` the result. Return `Nothing` if the cofinal type is less than the integer. Return `Nothing` if the integer is negative.
-
-  map (expand 2) (fromList [[0,0,0],[1,1,1]]) == map Just (fromList [[0,0],[1,1],[2,2]])
-  map (expand 0) (fromList [[0],[1],[2]]) == map Just (fromList [[0],[1]])
-  map (expand 2) (fromList [[0],[1],[2]]) == map Just (fromList [[0],[1],[1],[1])
-  map (expand 0) (fromList [[0],[0]]) == map Just (fromList [[0],[0]])
-  map (expand 1) (fromList [[0],[0]]) == Just Nothing
-  map (expand 0) (fromList []) == Just Nothing
-  map (expand -1) (fromList [[0],[1],[2]]) == Just Nothing
--}
-expand : Int -> Matrix -> Maybe Matrix
-expand n x = Just x
 
 type alias Model = { content : String }
 
