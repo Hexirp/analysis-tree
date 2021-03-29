a46b509b7ff8e8d80d6030c404ad6c7e674588f0
Hexirp <Hexirp@users.noreply.github.com>
2021-03-30 00:42:22 +0900

    Update: brackets と braces を切り出す
    他の EBNF をベースにした構文を使うモジュールでも使うと思われるため。

---
+++ src/Parser/Extra.elm
@@ -0,0 +1,28 @@
+module Parser.Extra exposing (brackets, braces)
+
+{-| パーサーについての便利な関数たちです。
+
+## 関数たち
+
+@docs brackets, braces
+
+-}
+
+import Parser exposing (..)
+
+{-| 省略可能な部分を表すパーサーです。
+
+EBNF における `[ ... ]` です。
+-}
+brackets : Parser a -> Parser (Maybe a)
+brackets x = oneOf [succeed Just |= x, succeed Nothing]
+
+{-| 省略可能かつ繰り返し可能な部分を表すパーサーです。
+
+EBNF における `{ ... }` です。
+-}
+braces : Parser a -> Parser (List a)
+braces x
+  =
+    oneOf
+      [succeed (::) |= x |= lazy (\_ -> braces x), succeed []]
