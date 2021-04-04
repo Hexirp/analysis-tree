28cd96c1019815f13c3865f24ffb14b2e107dad3
Hexirp <Hexirp@users.noreply.github.com>
2021-04-05 01:26:16 +0900

    Revert "Add: only を"
    This reverts commit 6023f07a8d1318afcb0bfefbd0f3c338d823b9f8.

--- tests/BMS_4_Tests.elm
+++ tests/BMS_4_Tests.elm
@@ -202,7 +202,7 @@ test_Patrix
       [
         test_calcPatrixFromMatrix,
         test_calcParentOnPatrixFromRawMatrix,
-        Test.only test_calcAncestorSetOnPatrixFromRawMatrix,
+        test_calcAncestorSetOnPatrixFromRawMatrix,
         test_calcMatrixFromPatrix,
         test_calcElementOnMatrixFromRawPatrix
       ]
