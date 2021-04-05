ff536caf8443240efa43e42fc1436a4108903432
Hexirp <Hexirp@users.noreply.github.com>
2021-04-06 00:16:05 +0900

    Update: BMS_4 モジュールを大整理する
    RawMatrix に関する関数を増やして、重複する意味を持つ関数を削除する。
    さらにモジュールの内容を単純にして、 Array を計算の中心にするため。

--- src/BMS_4.elm
+++ src/BMS_4.elm
@@ -1,26 +1,25 @@
 module BMS_4
   exposing
     (
-      fromArrayToList,
-      fromListToArray,
-      Nat,
+      Nat (..),
       RawMatrix,
-      Matrix,
-      fromMatrixToArray,
-      fromArrayToMatrix,
-      fromMatrixToList,
-      fromListToMatrix,
+      fromListToRawMatrix,
+      fromRawMatrixToList,
+      Matrix (..),
+      verifyMatrix,
+      fromMatrixToRawMatrix,
+      fromRawMatrixToMatrix,
       expand,
       Pindex (..),
       RawPatrix,
-      Patrix,
+      fromListToRawPatrix,
+      fromRawPatrixToList,
+      Patrix (..),
       calcPatrixFromMatrix,
       calcParentOnPatrixFromRawMatrix,
       calcAncestorSetOnPatrixFromRawMatrix,
       calcMatrixFromPatrix,
-      calcElementOnMatrixFromRawPatrix,
-      fromListToMatrixRawly,
-      fromListToPatrixRawly
+      calcElementOnMatrixFromRawPatrix
     )
 
 import Case exposing (Case (..))
@@ -29,14 +28,6 @@ import Array exposing (Array)
 import Array.Extra.Folding as Array
 
 import BMS_4.Parsing as Parsing
-
-{-| 或る配列を或るリストへ変換します。 -}
-fromArrayToList : Array (Array a) -> List (List a)
-fromArrayToList array = Array.toList (Array.map Array.toList array)
-
-{-| 或るリストを或る配列へ変換します。 -}
-fromListToArray : List (List a) -> Array (Array a)
-fromListToArray list = Array.map Array.fromList (Array.fromList list)
 
 {-| これは自然数です。 -}
 type Nat = Nat Int
@@ -47,9 +38,19 @@ type Nat = Nat Int
 -}
 type alias RawMatrix = Array (Array Int)
 
+{-| 或るリストを或る生の行列へ変換します。 -}
+fromListToRawMatrix : List (List Int) -> RawMatrix
+fromListToRawMatrix list = Array.map Array.fromList (Array.fromList list)
+
+{-| 或る生の行列を或るリストへ変換します。 -}
+fromRawMatrixToList : RawMatrix -> List (List Int)
+fromRawMatrixToList array = Array.toList (Array.map Array.toList array)
+
 {-| これはバシク行列システムにおける行列です。
 
 `Matrix` 型の値は `verifyMatrix` を満たしていなければなりません。すなわち、 `Matrix x y x_y_int` は、その行の長さが `x` であり、その列の長さが `y` でなければなりません。
+
+構築子は `Matrix` 型の規約が守られていることが保証されていないため、テスト以外で使ってはいけません。
 -}
 type Matrix = Matrix Int Int RawMatrix
 
@@ -64,36 +65,36 @@ verifyMatrix matrix
             && Array.length x_y_int == x
             && Array.all (\a -> Array.length a == y) x_y_int
 
-{-| 或る行列を或る配列へ変換します。 -}
-fromMatrixToArray : Matrix -> Array (Array Int)
-fromMatrixToArray matrix
-  =
-    case matrix of
-      Matrix x y x_y_int -> x_y_int
+{-| 或る生の行列を或る行列へ変換します。
 
-{-| 或る配列を或る行列へ変換します。
+其の生の行列が行列を正しく表していれば、そのようになります。もし、列の長さが不揃いであれば、最も長い列が行数の基準となり、それに合わせて他の列が底値でもってパディングされます。もし、行の長さがゼロであれば、行数（列の長さ）は不定値になりますので、デフォルトの値としてゼロとなります。
 
-其の配列が行列を正しく表していれば、そのようになります。もし、列の長さが不揃いであれば、最も長い列が行数の基準となり、それに合わせて他の列が底値でもってパディングされます。もし、行の長さがゼロであれば、行数（列の長さ）は不定値になりますので、デフォルトの値としてゼロとなります。
-
-底値は、其の配列に含まれる最小の値です。ゼロを使わないのは、其の配列が表現する行列のトポロジーを可能な限り保つためです。
+底値は、其の生の行列に含まれる最小の値です。ゼロを使わないのは、其の生の行列が表現する行列のトポロジーを可能な限り保つためです。
 -}
-fromArrayToMatrix : Array (Array Int) -> Matrix
-fromArrayToMatrix x_y_int
+fromRawMatrixToMatrix : RawMatrix -> Matrix
+fromRawMatrixToMatrix x_y_int
   =
     let
       x = Array.length x_y_int
-      y = Maybe.withDefault 0 (Array.maximum (Array.map Array.length x_y_int))
+      y
+        =
+          case Array.maximum (Array.map Array.length x_y_int) of
+            Nothing -> 0
+            Just y_ -> y_
       e
         =
-          Maybe.withDefault 0
+          case
             (Array.minimum
               (Array.map
                 (\y_int
                   ->
-                    Maybe.withDefault
-                      0
-                      (Array.minimum y_int))
+                    case Array.minimum y_int of
+                      Nothing -> 0
+                      Just e_ -> e_)
                 x_y_int))
+          of
+            Nothing -> 0
+            Just e_ -> e_
     in
       Matrix x y (Array.map (fromArrayToMatrix_helper_1 y e) x_y_int)
 
@@ -101,58 +102,12 @@ fromArrayToMatrix_helper_1 : Int -> Int -> Array Int -> Array Int
 fromArrayToMatrix_helper_1 y e y_int
   = Array.initialize y (\i -> Maybe.withDefault e (Array.get i y_int))
 
-{-| 或る行列を或るリストへと変換します。 -}
-fromMatrixToList : Matrix -> List (List Int)
-fromMatrixToList matrix
+{-| 或る行列を或る生の行列へ変換します。 -}
+fromMatrixToRawMatrix : Matrix -> RawMatrix
+fromMatrixToRawMatrix matrix
   =
     case matrix of
-      Matrix x y x_y_int -> fromArrayToList x_y_int
-
-{-| 或るリストを或る行列へと変換します。
-
-其のリストが行列を正しく表していれば、そのようになります。もし、列の長さが不揃いであれば、最も長い列が行数の基準となり、それに合わせて他の列が底値でもってパディングされます。もし、行の長さがゼロであれば、行数（列の長さ）は不定値になりますので、デフォルトの値としてゼロとなります。
-
-底値は、其のリストに含まれる最小の値です。ゼロを使わないのは、其のリストが表現する行列のトポロジーを可能な限り保つためです。
--}
-fromListToMatrix : List (List Int) -> Matrix
-fromListToMatrix x_y_int
-  =
-    let
-      x = List.length x_y_int
-      y = Maybe.withDefault 0 (List.maximum (List.map List.length x_y_int))
-      e
-        =
-          Maybe.withDefault 0
-            (List.minimum
-              (List.map
-                (\y_int
-                  ->
-                    Maybe.withDefault
-                      0
-                      (List.minimum y_int))
-                x_y_int))
-    in
-      Matrix
-        x
-        y
-        (Array.fromList (List.map (fromListToMatrix_helper_1 y e) x_y_int))
-
-fromListToMatrix_helper_1 : Int -> Int -> List Int -> Array Int
-fromListToMatrix_helper_1 y e y_int
-  = Array.initialize y (fromListToMatrix_helper_2 e y_int)
-
--- fromListToMatrix_helper_1 により 0 <= i である。
--- list-extra 8.3.0 の getAt で実装することも出来るが、上記の条件を使って実装を単純にしている。
-fromListToMatrix_helper_2 : Int -> List Int -> Int -> Int
-fromListToMatrix_helper_2 e y_int i
-  =
-    case y_int of
-      [] -> e
-      int :: y_int_
-        ->
-          if i == 0
-            then int
-            else fromListToMatrix_helper_2 e y_int_ (i - 1)
+      Matrix x y x_y_int -> x_y_int
 
 {-| 或る行列を或る自然数により展開します。 `Just` で包んだ結果を返します。其の自然数が其の行列の共終タイプ以上なら `Nothing` を返します。 -}
 expand : Matrix -> Nat -> Maybe Matrix
@@ -167,13 +122,23 @@ type Pindex = Null | Pindex Int
 -}
 type alias RawPatrix = Array (Array Pindex)
 
+{-| 或るリストを或る生のパトリックスへ変換します。 -}
+fromListToRawPatrix : List (List Pindex) -> RawPatrix
+fromListToRawPatrix list = Array.map Array.fromList (Array.fromList list)
+
+{-| 或る生のパトリックスを或るリストへ変換します。 -}
+fromRawPatrixToList : RawPatrix -> List (List Pindex)
+fromRawPatrixToList array = Array.toList (Array.map Array.toList array)
+
 {-| これはパトリックスです。パトリックスはピンデックスの行列を意味します。
 
 `Patrix x y x_y_pindex` は、その行の長さが `x` であり、その列の長さが `y` であり、それぞれの行が正しい木構造を構成していて、上部の行の木構造に下部の行の木構造が埋め込み可能でなければなりません。
+
+構築子は `Patrix` 型の規約が守られていることが保証されていないため、テスト以外で使ってはいけません。
 -}
 type Patrix = Patrix Int Int RawPatrix
 
-{-| 或る行列をパトリックスへ変換します。 -}
+{-| 或る行列から或るパトリックスを計算します。 -}
 calcPatrixFromMatrix : Matrix -> Case Patrix
 calcPatrixFromMatrix matrix
   =
@@ -318,7 +283,7 @@ calcAncestorSetOnPatrixFromRawMatrix x_y_int x y
               else
                 PossibleCase (\x__ -> x == x__)
 
-{-| 或るパトリックスを或る行列に変換します。 -}
+{-| 或るパトリックスから或る行列を計算します。 -}
 calcMatrixFromPatrix : Patrix -> Case Matrix
 calcMatrixFromPatrix patrix
   =
@@ -387,17 +352,3 @@ calcElementOnMatrixFromRawPatrix x_y_pindex x y
                             ImpossibleCase -> ImpossibleCase
                             PossibleCase int -> PossibleCase (int + 1)
                         else PossibleCase 0
-
-{-| 或るリストを生のまま行列へ変換します。
-
-この関数は `Matrix` の規約を破る値を返す可能性があります。
--}
-fromListToMatrixRawly : Int -> Int -> List (List Int) -> Matrix
-fromListToMatrixRawly x y x_y_int = Matrix x y (fromListToArray x_y_int)
-
-{-| 或るリストを生のままパトリックスへ変換します。
-
-この関数は `Patrix` の規約を破る値を返す可能性があります。
--}
-fromListToPatrixRawly : Int -> Int -> List (List Pindex) -> Patrix
-fromListToPatrixRawly x y x_y_pindex = Patrix x y (fromListToArray x_y_pindex)
