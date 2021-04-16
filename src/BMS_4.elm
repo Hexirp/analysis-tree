c1d5cffd718ad3b52527147b8bd9889a8284ca59
Hexirp <Hexirp@users.noreply.github.com>
2021-04-16 21:13:04 +0900

    Fix: コピーを司る計算式を修正する

--- src/BMS_4.elm
+++ src/BMS_4.elm
@@ -809,7 +809,7 @@ expandPatrix_helper_3 x y x_y_pindex xr x_ y_
                   Just pindex -> PossibleCase pindex
         else
           let
-            n = modBy (x - xr) (x_ - xr)
+            n = modBy ((x - 1) - xr) (x_ - xr)
           in
             if n == 0
               then
