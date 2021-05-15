c5f9176fc6c88336f83476845aa3714636fbb1c3
Hexirp <Hexirp@users.noreply.github.com>
2021-05-15 17:59:41 +0900

    Add: Notation モジュールのテストを

---
+++ tests/Notation_Tests.elm
@@ -0,0 +1,46 @@
+module Notation_Tests
+  exposing
+    (
+      test_Nat
+    ,
+      test_Coftype
+    )
+
+import Case exposing (Case (..))
+
+import Notation exposing (..)
+
+import Test exposing (Test, describe, test)
+
+-- Nat
+
+test_Nat :: Test
+test_Nat
+  =
+    describe "Nat"
+      [
+        test_zero
+      ,
+        test_succ
+      ,
+        test_toNatFromInt
+      ,
+        test_toIntFromNat
+      ]
+
+test_zero :: Test
+test_zero
+  =
+    describe "zero"
+      [
+        let
+          func _
+            =
+              let
+                target = zero
+                expect = Nat 0
+              in
+                target |> Expect.equal expect
+        in
+          test "normal case" func
+      ]
