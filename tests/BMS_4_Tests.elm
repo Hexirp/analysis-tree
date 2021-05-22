f490ec5af3da9cb9831d0213b360c0327aea66db
Hexirp <Hexirp@users.noreply.github.com>
2021-05-22 17:04:54 +0900

    Add: BMS_4_Util モジュールを
    汎用的な定義を別のモジュールへと分ける。

--- tests/BMS_4_Tests.elm
+++ tests/BMS_4_Tests.elm
@@ -24,105 +24,10 @@ import Test exposing (Test, describe, test, fuzz, fuzz2, fuzz3)
 
 import Notation_Util exposing (fuzzer_Nat)
 
+import BMS_4_Util exposing (..)
+
 expect_notImpossibleCase : Case a -> Expectation
 expect_notImpossibleCase = Expect.notEqual ImpossibleCase
-
-fuzzer_RawMatrix : Fuzzer RawMatrix
-fuzzer_RawMatrix = Fuzz.array (Fuzz.array Fuzz.int)
-
-fuzzer_Matrix : Fuzzer Matrix
-fuzzer_Matrix
-  =
-    let
-      generator
-        =
-          Random.map3 Matrix
-            (Random.int -100 100)
-            (Random.int -100 100)
-            (Random.map toRawMatrixFromList
-              (Random.list 10
-                (Random.list 10
-                  (Random.int -100 100))))
-      shrinker matrix
-        =
-          case matrix of
-            Matrix x y x_y_int
-              ->
-                Shrink.map Matrix (Shrink.int <| x)
-                  |> Shrink.andMap (Shrink.int <| y)
-                  |>
-                    Shrink.andMap
-                      (Shrink.array (Shrink.array Shrink.int) <| x_y_int)
-    in
-      Fuzz.custom generator shrinker
-
-fuzzer_Pindex : Fuzzer Pindex
-fuzzer_Pindex
-  =
-    let
-      generator
-        =
-          Random.weighted (1, True) [(10, False)]
-            |>
-              Random.andThen
-                (\bool
-                  ->
-                    if bool
-                      then Random.constant Null
-                      else Random.map Pindex (Random.int -100 100))
-      shrinker pindex
-        =
-          case pindex of
-            Null -> Shrink.noShrink Null
-            Pindex int -> Shrink.map Pindex (Shrink.int int)
-    in
-      Fuzz.custom generator shrinker
-
-fuzzer_RawPatrix : Fuzzer RawPatrix
-fuzzer_RawPatrix = Fuzz.array (Fuzz.array fuzzer_Pindex)
-
-fuzzer_Patrix : Fuzzer Patrix
-fuzzer_Patrix
-  =
-    let
-      generator_Pindex
-        =
-          Random.weighted (1, True) [(10, False)]
-            |>
-              Random.andThen
-                (\bool
-                  ->
-                    if bool
-                      then Random.constant Null
-                      else Random.map Pindex (Random.int -100 100))
-      shrinker_Pindex pindex
-        =
-          case pindex of
-            Null -> Shrink.noShrink Null
-            Pindex int -> Shrink.map Pindex (Shrink.int int)
-      generator
-        =
-          Random.map3 Patrix
-            (Random.int -100 100)
-            (Random.int -100 100)
-            (Random.map toRawPatrixFromList
-              (Random.list 10
-                (Random.list 10
-                  generator_Pindex)))
-      shrinker patrix
-        =
-          case patrix of
-            Patrix x y x_y_pindex
-              ->
-                Shrink.map Patrix (Shrink.int <| x)
-                  |> Shrink.andMap (Shrink.int <| y)
-                  |>
-                    Shrink.andMap
-                      (Shrink.array (Shrink.array shrinker_Pindex)
-                        <|
-                          x_y_pindex)
-    in
-      Fuzz.custom generator shrinker
 
 test_Matrix : Test
 test_Matrix
