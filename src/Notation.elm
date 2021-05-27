173c63b83cb9b0b915902f9b90db6a67663450e4
Hexirp <Hexirp@users.noreply.github.com>
2021-05-28 00:42:28 +0900

    Fix: toRawOuterFromTerm を
    True と False が正しい処理と逆だったので。
    
    toRawOuterFromTerm のテストの中でエラーにぶつかって落ちるのを、
    出来るだけエラーを遅らせるように考えて、見つけたバグである。

--- src/Notation.elm
+++ src/Notation.elm
@@ -198,9 +198,9 @@ toRawOuterFromTerm_helper_2 notation term x_int term_ nat
               ->
                 case
                   case notation.compare term term__ of
-                    LT -> True
-                    EQ -> True
-                    GT -> False
+                    LT -> False
+                    EQ -> False
+                    GT -> True
                 of
                   True -> toRawOuterFromTerm_helper_1 notation term (Array.push (toIntFromNat nat) x_int) term__
                   False -> toRawOuterFromTerm_helper_2 notation term x_int term__ (succ nat)
