5ea4bee64138e37807ff8319d61265a3be31a5bd
Hexirp <Hexirp@users.noreply.github.com>
2021-06-04 20:56:35 +0900

    Add: BMS_4_Printing モジュールを
    [1] を解決するため。 BMS_4 の項を表示するのに必要になるため。
    
    [1]: https://gitlab.com/Hexirp/analysis-tree/-/issues/13
    
    See also: #13

---
+++ src/BMS_4_Printing.elm
@@ -0,0 +1,31 @@
+module BMS_4_Printing
+  exposing
+    (
+      printRawMatrix
+    ,
+      printMatrix
+    )
+
+import Array
+
+import String
+
+import BMS_4
+
+printRawMatrix : BMS_4.RawMatrix -> String
+printRawMatrix x_y_int = Array.foldl (++) "" (Array.map printRawMatrix_helper_1 x_y_int)
+
+printRawMatrix_helper_1 : Array Int -> String
+printRawMatrix_helper_1 y_int
+  =
+    let
+      func int (string, bool)
+        =
+          if bool
+            then (string ++ "," ++ String.fromInt int, True)
+            else (string ++ String.fromInt int, True)
+    in
+      "(" ++ Array.foldl func ("", False) y_int ++ ")"
+
+printMatrix : BMS_4.Matrix -> String
+printMatrix matrix = printRawMatrix (BMS_4.toRawMatrixFromMatrix matrix)
